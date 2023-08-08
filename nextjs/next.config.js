b3a3c7cb87e0f400c2fd3e46e0ade6d3d3f5ca9a
Royalo04 <filip.sankowski@gmail.com>
2023-08-08 10:57:23 +0200

    changed nextjs connection to env file

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,12 +1,5 @@
-const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-const { parsed: myEnv } = require('dotenv').config({
+dotenv.config({
   path:'../.env'
 });
-
-module.exports = {
-  webpack(config) {
-      config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-      return config;
-  }
-}
